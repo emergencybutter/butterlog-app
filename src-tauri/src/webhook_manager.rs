6ef4e40a4cb5e7bf329303ac07c19727e88cdfbf
Arnaud cornet <arnaud.cornet@gmail.com>
2026-05-29 11:26:33 -0400

    Print actual peer list addresses in client webhook logs

--- src-tauri/src/webhook_manager.rs
+++ src-tauri/src/webhook_manager.rs
@@ -145,7 +145,7 @@ impl WebhookManager {
                             *self.last_update_time.lock().unwrap() = Some(now);
                             if let Ok(data) = res.json::<WebhookFlightResponse>().await {
                                 if let Some(peers) = data.peers {
-                                    crate::append_log(app, format!("[Webhook] Received {} peers from service", peers.len()));
+                                    crate::append_log(app, format!("[Webhook] Received {} peers from service: {:?}", peers.len(), peers));
                                     if let Some(multiplayer) = app.try_state::<Arc<crate::multiplayer::MultiplayerManager>>() {
                                         multiplayer.update_peers(peers);
                                     }
@@ -178,7 +178,7 @@ impl WebhookManager {
                                 *self.last_update_time.lock().unwrap() = Some(now);
 
                                 if let Some(peers) = data.peers {
-                                    crate::append_log(app, format!("[Webhook] Received {} peers from service", peers.len()));
+                                    crate::append_log(app, format!("[Webhook] Received {} peers from service: {:?}", peers.len(), peers));
                                     if let Some(multiplayer) = app.try_state::<Arc<crate::multiplayer::MultiplayerManager>>() {
                                         multiplayer.update_peers(peers);
                                     }
